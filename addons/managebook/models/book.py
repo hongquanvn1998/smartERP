4fa8561e9ad17220b525f98b9b6bbb846bd4f183
unknown <hongquanvn1998@gmail.com>
2019-06-19 19:44:02 +0700

    update model managebook

--- addons/managebook/models/book.py
+++ addons/managebook/models/book.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models,fields
+from odoo import models,fields,api
+from odoo.exceptions import ValidationError
 
 class Book(models.Model):
     _name = "managebook.mybook"
     _rec_name = "name_book" #tên hiển thị trên quan hệ many2many, one2many, many2one
     _auto = True #tự động tạo bảng nếu bằng True
-    _table = u'Các cuốn sách' #tên hiển thị khi truy cập vào bảng
+    #_table = u'Các cuốn sách' #tên hiển thị khi truy cập vào bảng
     _description = u'Sách của tui' #mô tả bảng
-    _order = 'author_main,name_book' #sắp xếp thứ tự các trường trong bảng
+    _order = 'seri_num,author_main,name_book' #sắp xếp thứ tự các trường trong bảng
     _translate = True #cho phép dịch sang ngôn ngữ khác
 
-    name_book = fields.Char(string="Tên sách", size=200,translate=True,default="")
+    seri_num = fields.Char(string="Mã sách",required=True)
+    name_book = fields.Char(string="Tên sách",required=True, size=200,translate=True,default="")
     image = fields.Binary("Hình ảnh")
     date_publication = fields.Date("Ngày xuất bản")
     quantum =  fields.Integer(string="Số lượng")
@@ -26,5 +28,16 @@ class Book(models.Model):
     author_2nd = fields.Many2one(comodel_name="managebook.author",string="Tác giả phụ")
 
 
-    _sql_constraints = {('ten_sach_la_duy_nhat','UNIQUE(name_book)',u'Sách bạn tạo đã tồn tại vui lòng thử lại')}
+    _sql_constraints = {('ten_sach_la_duy_nhat','UNIQUE(name_book)',u'Sách bạn tạo đã tồn tại vui lòng thử lại'),('ma_sach_duy_nhat','UNIQUE(seri_num)',u'Mã sách bạn tạo đã đồn tồn tại')}
     _depends = {} #truy xuất dữ liệu đến các bảng để lấy fields,model
+
+    @api.multi
+    @api.constrains('name_book')
+    def _check_name_book(self):
+        for rec in self:
+            if len(rec.name_book) < 2:
+                raise ValidationError("Tên sách quá ngắn")
+
+    @api.onchange('author_main')
+    def _add_author_in_namebook(self):
+        self.name_book = self.name_book + " = "+ self.author_main.name
